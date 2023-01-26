2166622c5eab217bc57ffb9cd2cc969e25bc714e
Stellanyambura <stellagitonga.com>
2023-01-26 22:19:20 +0300

    fixed bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,111 +1,70 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import Home from './Home'
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        MEMESAPP
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-const Nav=()=>{
-    return(
-    <nav>
-        <NavLink to="/loginpage">LoginPage</NavLink>
-        <NavLink exact="true" to="/randommemes">RandomMemes</NavLink>
-      
-</nav>
-    )
-  }
-  export default Nav
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Navbar = () => {
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <a className="navbar-brand" href="#">
-//         MEMESAPP
-//       </a>
-//       <button
-//         className="navbar-toggler"
-//         type="button"
-//         data-toggle="collapse"
-//         data-target="#navbarNavDropdown"
-//         aria-controls="navbarNavDropdown"
-//         aria-expanded="false"
-//         aria-label="Toggle navigation"
-//       >
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-
-//       <div className="collapse navbar-collapse" id="navbarNavDropdown">
-//         <ul className="navbar-nav">
-//           <li className="nav-item active">
-//             <a className="nav-link" href="#">
-//               MEMES<span className="sr-only">(LIST)</span>
-//             </a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">
-//               MEMELIST 
-//             </a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              MEMES<span className="sr-only">(LIST)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              MEMELIST 
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
         
-//             </a>
-//           </li>
-//           <li className="nav-item dropdown">
-//             <a
-//               className="nav-link dropdown-toggle"
-//               href="#"
-//               id="navbarDropdownMenuLink"
-//               data-toggle="dropdown"
-//               aria-haspopup="true"
-//               aria-expanded="false"
-//             >
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
             
-//             </a>
-//             <div
-//               className="dropdown-menu"
-//               aria-labelledby="navbarDropdownMenuLink"
-//             >
-//               <a className="dropdown-item" href="#">
-//                 Action
-//               </a>
-//               <a className="dropdown-item" href="#">
-//                 Another action
-//               </a>
-//               <a className="dropdown-item" href="#">
-//                 Something else here
-//               </a>
-//             </div>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// };
-// export default Navbar;
+            </a>
+            <div
+              className="dropdown-menu"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
 
